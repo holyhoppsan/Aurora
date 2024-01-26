47ca732c5ab32720af0ab2279836233e7964d9f7
Daniel Hall <daniel.hall@live.se>
2024-01-26 15:51:35 +0000

    Added ability for two cursors

--- Assets/AuroraInstanceController.cs
+++ Assets/AuroraInstanceController.cs
@@ -10,6 +10,9 @@ public class AuroraInstanceController : MonoBehaviour
     private Transform _leftCursorTransform;
 
     [SerializeField]
+    private Transform _rightCursorTransform;
+
+    [SerializeField]
     private InputManager _inputManager;
 
     [SerializeField]
@@ -17,6 +20,9 @@ public class AuroraInstanceController : MonoBehaviour
 
     [SerializeField]
     public float fadeIntensity;
+
+    [SerializeField]
+    public bool _debugMode = false;
 
     // Start is called before the first frame update
     void Start()
@@ -27,12 +33,34 @@ public class AuroraInstanceController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 leftCursorPosition = new Vector3();
+        Vector3 rightCursorPosition = new Vector3();
+
+        if (_debugMode)
+        {
+            leftCursorPosition = _leftCursorTransform.position;
+            rightCursorPosition = _rightCursorTransform.position;
+        }
+        else
+        {
+            leftCursorPosition = GetHitPosition(_inputManager.LeftCursorScreenPosition);
+            rightCursorPosition = GetHitPosition(_inputManager.RightCursorScreenPosition);
+        }
+
+        Renderer auroraRenderer = GetComponent<Renderer>();
+        auroraRenderer.material.SetFloat("_Radius", _radius);
+        auroraRenderer.material.SetVector("_LeftCursorPoint", leftCursorPosition);
+        auroraRenderer.material.SetVector("_RightCursorPoint", rightCursorPosition);
+        auroraRenderer.material.SetFloat("_FadeIntensity", fadeIntensity);
+
+    }
+
+    private Vector3 GetHitPosition(Vector2 screenPos)
+    {
         Ray ray = Camera.main.ScreenPointToRay(_inputManager.LeftCursorScreenPosition);
         RaycastHit hit;
 
-        //Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100.0f);
-
-        Vector4 leftCursorPosition = new Vector3();
+        Vector3 leftCursorPosition = new Vector3();
 
         if (Physics.Raycast(ray, out hit))
         {
@@ -42,10 +70,6 @@ public class AuroraInstanceController : MonoBehaviour
             }
         }
 
-        Renderer auroraRenderer = GetComponent<Renderer>();
-        auroraRenderer.material.SetFloat("_Radius", _radius);
-        auroraRenderer.material.SetVector("_CursorPoint", leftCursorPosition);
-        auroraRenderer.material.SetFloat("_FadeIntensity", fadeIntensity);
-
+        return leftCursorPosition;
     }
 }
