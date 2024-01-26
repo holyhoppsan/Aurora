4385780ae6ca7cb097d8d73bb7a31f29978bf813
Daniel Hall <daniel.hall@live.se>
2024-01-26 02:28:01 +0000

    Added timer to Aurora flora state

--- Assets/Scripts/States/AuroraAndFloraState.cs
+++ Assets/Scripts/States/AuroraAndFloraState.cs
@@ -10,15 +10,23 @@ public class AuroraAndFloraState : FSMState
     [SerializeField]
     private CameraController _cameraController;
 
+    [SerializeField]
+    private float _stateDuration = 3.0f;
+
+    private float _currentTimer;
+
 
     public override void Enter()
     {
         _cameraController.SwitchCamera("FlowerZoomInView");
+        _currentTimer = 0.0f;
     }
 
     public override void Tick()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        _currentTimer += Time.deltaTime;
+
+        if (_currentTimer >= _stateDuration || Input.GetKeyDown(KeyCode.Space))
         {
             _fsm.Transition<EndTransitionState>();
         }
