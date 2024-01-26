390a1c095986a37d661ce6ff769e9356971d39aa
Daniel Hall <daniel.hall@live.se>
2024-01-26 00:59:36 +0000

    Aurora shader graph displacement test

--- Assets/AuroraInstanceController.cs
+++ Assets/AuroraInstanceController.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShortcutManagement;
 using UnityEngine;
 
 public class AuroraInstanceController : MonoBehaviour
 {
+
+    [SerializeField]
+    private Transform _leftCursorTransform;
+
+    [SerializeField]
+    private float _radius;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +22,8 @@ public class AuroraInstanceController : MonoBehaviour
     void Update()
     {
         Renderer auroraRenderer = GetComponent<Renderer>();
-        auroraRenderer.material.SetMatrix("_WorldToLocalMatrix", transform.worldToLocalMatrix);
+        auroraRenderer.material.SetFloat("_Radius", _radius);
+        auroraRenderer.material.SetVector("_CursorPoint", _leftCursorTransform.position);
+
     }
 }
